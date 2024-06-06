dcfd9d9c2a0dc109ca155cbe858b7cfabdc31d8b
Bibibush <rlawlsgus125@gmail.com>
2024-06-06 20:43:33 +0900

    해치웠나용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -141,7 +141,7 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
       <Text color="orange" style={headingStyles}>
-        해치웠나
+        해치웠나용?
         <br />
         <span style={headingAccentStyles}>
           — you just made a Gatsby site! 🎉🎉🎉
