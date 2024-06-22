1392941dcd385fb2a6ad4c4df69561373320392c
Bibibush <rlawlsgus125@gmail.com>
2024-06-22 22:30:58 +0900

    조금의 커스터마이징

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import type { HeadFC, PageProps } from "gatsby";
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <h1 className="text-xl font-bold">Welcome to my Gatsby site!</h1>
+      <h1 className="text-2xl font-bold">살로메 사랑해</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   );
